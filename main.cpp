ae970307c92484e412ffeb774f52c9db8ea20410
Luis322 <37373448+Luis322@users.noreply.github.com>
2018-03-21 09:59:18 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
     cout << "Hello world!" << endl;
     cout << "Luis Alberto Palomino Benique" << endl;
-    cout <<" jhorel revilla calderon" << endl;
+    cout << "jhorel revilla calderon" << endl;
+
+
+
+    //x = 1
+    //int y;
+    //double x,z,d,r,pi;
+    //pi = 3.1416;
+    //cout <<"X = ";
+    //cout << "Radio = ";
+    //cin >> x;
+    //cout <<"Y = ";
+    //cin >> y;
+    //z = x+y;
+    //d = x/y;
+    //cout << z << "--" << d << endl;
+    //r = pi * (x*x);
+    //cout << "El area es \n" << r << "\n" << pi  << endl;
+
+    cout << "*********"<<"\t"<<"***"<<endl;
+    cout << "*       *"<<"\t"<<"   *"<< endl;
+
+
+
+
     return 0;
+
 }
