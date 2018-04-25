982b42c92eb13bcd07748d210acbe8914b3e3555
Luis322 <37373448+Luis322@users.noreply.github.com>
2018-04-25 08:10:53 -0500

    Add files via upload

---
+++ practica punteros.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int esMayor(int e){
+    int mayMe[2]={1,0};
+    int *p;
+    if(e<18){
+        int p=mayMe[1];
+    }else{
+        int p=mayMe[0];
+    }
+}
+
+
+int main()
+{
+    cout<<esMayor(17);
+
+    return 0;
+}
